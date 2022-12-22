fdab463bb63cc5b38d53e2d4f5a58bd190730d7e
BlueVegetable <2601042086@qq.com>
2022-12-22 17:14:20 +0800

    2022-12-22

--- src/LeetCode0191.java
+++ src/LeetCode0191.java
@@ -1,5 +1,21 @@
+/**
+ * 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+ */
 public class LeetCode0191 extends BaseLeetCode {
     public static void main(String[] args) {
-        ;
+        solution(new LeetCode0191().hammingWeight(11));
+    }
+
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int length = 32;
+        int result = 0;
+        for (int i=0;i<length;i++) {
+            if((n&1) != 0) {
+                result++;
+            }
+            n >>>= 1;
+        }
+        return result;
     }
 }
