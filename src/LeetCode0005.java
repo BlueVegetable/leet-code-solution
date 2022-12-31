c3aa5a87452519c6bad0c5b2f034e73e089bd1e1
BlueVegetable <2601042086@qq.com>
2022-12-31 21:51:37 +0800

    2022-12-31

--- src/LeetCode0005.java
+++ src/LeetCode0005.java
@@ -3,7 +3,7 @@
  */
 public class LeetCode0005 extends BaseLeetCode {
     public static void main(String[] args) {
-        ;
+        solution(new LeetCode0005().longestPalindrome("aaaa"));
     }
 
     /**
@@ -12,27 +12,50 @@ public class LeetCode0005 extends BaseLeetCode {
      * @return
      */
     public String longestPalindrome(String s) {
-        int length = s.length();
-        int maxLength = -1;
-        String result = "";
-        char[] chs = s.toCharArray();
-        boolean[][] check = new boolean[length][length];
-        for (int i=0;i<length;i++) {
-            //每一个单个字符均为回文串
-            check[i][i] = true;
+        int len = s.length();
+        if (len < 2) {
+            return s;
         }
-        for (int i=0;i<length;i++) {
-            if(i == length - 1) {
-                break;
-            }
-            for (int j=i+1;j<length;j++) {
-                boolean flag = false
-                if(Math.abs(i-j) == 1) {
-                    flag = chs[i] == chs[j];
+
+        int maxLen = 1;
+        int begin = 0;
+        // dp[i][j] 表示 s[i..j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        // 初始化：所有长度为 1 的子串都是回文串
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        char[] charArray = s.toCharArray();
+        // 递推开始
+        // 先枚举子串长度
+        for (int L = 2; L <= len; L++) {
+            // 枚举左边界，左边界的上限设置可以宽松一些
+            for (int i = 0; i < len; i++) {
+                // 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+                int j = L + i - 1;
+                // 如果右边界越界，就可以退出当前循环
+                if (j >= len) {
+                    break;
+                }
+
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
                 } else {
-                    flag = check[i+1][j-1]
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
                 }
             }
         }
+        return s.substring(begin, begin + maxLen);
     }
 }
