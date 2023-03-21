58b4ac5cfd3313ceb9f21b9ae70edbff6ed1d21f
BlueVegetable <2601042086@qq.com>
2023-03-21 20:58:52 +0800

    2023-03-21

---
+++ src/LeetCode0142.java
@@ -0,0 +1,24 @@
+/**
+ * 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ */
+public class LeetCode0142 extends BaseLeetCode {
+    public static void main(String[] args) {
+        ;
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (true) {
+            if (fast == null || fast.next == null) return null;
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) break;
+        }
+        fast = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+}
